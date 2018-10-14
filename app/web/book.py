0ce3a9bca7877fc58b497b3960da23ffbb8700c1
245967906 <245967906@qq.com>
2018-10-14 12:32:32 +0800

    4-4、request 对象

--- app/web/book.py
+++ app/web/book.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from flask import jsonify
+from flask import jsonify, request
 from helper import is_isbn_or_key
 from yushu import YuShu
 from . import web
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
+@web.route('/book/search')
+def search():
+    q = request.args['q']
+    page = request.args['page']
     isbn_or_key = is_isbn_or_key(q)
     if isbn_or_key == 'isbn':
         result = YuShu.search_by_isbn(q)
