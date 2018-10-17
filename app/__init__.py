258038224de36db99796c9fbaf7a16926546c6dd
245967906 <245967906@qq.com>
2018-10-17 19:22:38 +0800

    4-10、ORM与CodeFirst区别

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,10 @@
 
 from flask import Flask
 from app.web import web
+from app.models.book import db
 
 app = Flask(__name__)
 app.config.from_object('app.config')
 app.register_blueprint(web)
+db.init_app(app)
+db.create_all(app=app)
--- app/config.py
+++ app/config.py
@@ -2,3 +2,4 @@
 
 DEBUG = False
 PER_PAGE = 5
+SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1:3306/fisher'
