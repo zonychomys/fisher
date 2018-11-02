6de380872530c2a85b5b3ff7410afe54e3a4daf5
245967906 <245967906@qq.com>
2018-11-02 10:27:34 +0800

    7-7、从json序列化看代码解释权反转

--- app/view_models/books.py
+++ app/view_models/books.py
@@ -1,34 +1,27 @@
 # -*- coding: utf-8 -*-
+"""
+对传入的数据进行过滤，提取真正需要的字段
+"""
 
 
-class BookViewModel(object):
-    @classmethod
-    def package_single(cls, data, keyword):
-        returned = {'books': [], 'total': 0, 'keyword': keyword}
-        if data:
-            returned['total'] = 1
-            returned['books'] = [cls._cut_book_data(data)]
-        return returned
+class BookViewModel:
+    def __init__(self, book):
+        self.title = book['title']
+        self.publisher = book['publisher']
+        self.pages = book['pages'] or ''
+        self.author = '、'.join(book['author'])
+        self.price = book['price']
+        self.summary = book['summary'] or ''
+        self.image = book['image']
 
-    @classmethod
-    def package_collection(cls, data, keyword):
-        returned = {'books': [], 'total': 0, 'keyword': keyword}
-        if data:
-            returned['total'] = data['total']
-            returned['books'] = [
-                cls._cut_book_data(book) for book in data['books']
-            ]
-        return returned
 
-    @classmethod
-    def _cut_book_data(cls, data):
-        book = {
-            'total': data['title'],
-            'publisher': data['publisher'],
-            'pages': data['pages'] or '',
-            'author': '、'.join(data['author']),
-            'price': data['price'],
-            'summary': data['summary'] or '',
-            'image': data['image']
-        }
-        return book
+class BookCollection:
+    def __init__(self):
+        self.total = 0
+        self.books = []
+        self.keyword = ''
+
+    def fill(self, yushu_book, keyword):
+        self.total = yushu_book.total
+        self.keyword = keyword
+        self.books = [BookViewModel(book) for book in yushu_book.books]
--- app/web/book.py
+++ app/web/book.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 from flask import jsonify, request
 from app.forms.book import SearchForm
 from app.lib.helper import is_isbn_or_key
 from app.spider.yushu import YuShu
-from app.view_models.books import BookViewModel
+from app.view_models.books import BookCollection
 from . import web
 
 
 @web.route('/book/search')
 def search():
     form = SearchForm(request.args)
+    books = BookCollection()
     if form.validate():
         q = form.q.data.strip()
         page = form.page.data
         isbn_or_key = is_isbn_or_key(q)
+        yushu = YuShu()
         if isbn_or_key == 'isbn':
-            result = YuShu.search_by_isbn(q)
-            result = BookViewModel.package_single(result, q)
+            yushu.search_by_isbn(q)
         else:
-            result = YuShu.search_by_keyword(q, page)
-            result = BookViewModel.package_collection(result, q)
-        return jsonify(result)
+            yushu.search_by_keyword(q, page)
+        books.fill(yushu, q)
+        return json.dumps(books, default=lambda x: x.__dict__)
     else:
         return jsonify({'msg': form.errors})
