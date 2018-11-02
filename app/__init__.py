bc82d7767ca7278217f5b409f6dbdb32cd31fe8b
245967906 <245967906@qq.com>
2018-11-02 11:32:54 +0800

    8-1、静态文件访问原理

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask import Flask
 from app.web import web
 from app.models.book import db
 
-app = Flask(__name__)
+# 该语句决定了应用程序app的根目录是app而不是fisher
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 app.config.from_object('app.config')
 app.register_blueprint(web)
 db.init_app(app)
