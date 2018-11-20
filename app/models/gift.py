3140151996572769084b2e25dab8e97181d0e649
zl13 <zl13@us.meitu.com>
2018-11-20 17:25:30 +0800

    11-1、最近的礼物（复杂SQL的编写方案）

--- app/models/gift.py
+++ app/models/gift.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, String, Integer, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from app.models.base import Base
+from flask import current_app
 
 
 class Gift(Base):
@@ -11,3 +12,9 @@ class Gift(Base):
     uid = Column(Integer, ForeignKey('user.id'), nullable=False)
     isbn = Column(String(13))
     launched = Column(Boolean, default=False)
+
+    def recent(self):
+        recent_gift = Gift.query.filter_by(launched=False).group_by(
+            Gift.isbn).order_by(Gift.create_time).limit(
+                current_app.config['RECENT_BOOK_COUNT']).all()
+        return recent_gift
